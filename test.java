66f8e62a6bba7e1d3e01738f3eb68a48528a29a2
RAM NAIK <ramnaik768@gmail.com>
2026-02-02 20:50:56 +0530

    Normalize line endings

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+       for (String str : strs) {
+          for(int i=0;i<str.length();i++){
+             if(str.charAt(i)==){}
+          }
+       }
+    }
+}
